4730963d447f5909d7f0a9ff31bbea980f58b808
Dev_likit_acer <likityodya@gmail.com>
2016-09-02 01:38:56 +0700

    v1

--- application/views/upload/upload.php
+++ application/views/upload/upload.php
@@ -1,173 +1,143 @@
-<base href="<?php echo base_url(); ?>">
-<!-- Bootstrap styles -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-<!-- Generic page styles -->
-<link rel="stylesheet" href="assets/css/fileupload/style.css">
-<!-- blueimp Gallery styles -->
-<link rel="stylesheet" href="https://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-<!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
-<link rel="stylesheet" href="assets/css/fileupload/jquery.fileupload.css">
-<link rel="stylesheet" href="assets/css/fileupload/jquery.fileupload-ui.css">
-<!-- CSS adjustments for browsers with JavaScript disabled -->
-<noscript><link rel="stylesheet" href="assets/css/fileupload/jquery.fileupload-noscript.css"></noscript>
-<noscript><link rel="stylesheet" href="assets/css/fileupload/jquery.fileupload-ui-noscript.css"></noscript>
-<!-- The file upload form used as target for the file upload widget -->
-<form id="fileupload" action="upload/do_upload" method="POST" enctype="multipart/form-data">
-    <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-    <div class="row fileupload-buttonbar">
-        <div class="col-lg-7">
-            <!-- The fileinput-button span is used to style the file input field as button -->
-            <span class="btn btn-success fileinput-button">
-                <i class="glyphicon glyphicon-plus"></i>
-                <span>Add files...</span>
-                <input type="file" name="userfile" multiple>
-            </span>
-            <button type="submit" class="btn btn-primary start">
-                <i class="glyphicon glyphicon-upload"></i>
-                <span>Start upload</span>
-            </button>
-            <button type="reset" class="btn btn-warning cancel">
-                <i class="glyphicon glyphicon-ban-circle"></i>
-                <span>Cancel upload</span>
-            </button>
-            <button type="button" class="btn btn-danger delete">
-                <i class="glyphicon glyphicon-trash"></i>
-                <span>Delete</span>
-            </button>
-            <input type="checkbox" class="toggle">
-            <!-- The global file processing state -->
-            <span class="fileupload-process"></span>
-        </div>
-        <!-- The global progress state -->
-        <div class="col-lg-5 fileupload-progress fade">
-            <!-- The global progress bar -->
-            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-            </div>
-            <!-- The extended global progress state -->
-            <div class="progress-extended">&nbsp;</div>
+<!doctype html>
+<html>
+<head>
+    <!-- Metro UI bootstrap-->
+    <link href="<?php echo base_url("assets/css/metro.css")?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/metro-responsive.css")?>" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/metro-icons.css");?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/metro-schemes.css");?>">
+    <!-- END bootstrap-->
+    <!-- Dropzone CSS-->
+    <link href="<?php echo base_url("assets/plugin/dropzone/min/dropzone.min.css")?>" rel="stylesheet">
+    <!-- End Css Dropzone-->
+    <!-- Load javascript-->
+    <script src="<?php echo base_url("assets/js/jquery-2.1.3.min.js");?>"></script>
+    <script src="<?php echo base_url("assets/plugin/dropzone/dropzone.js")?>"></script>
+    <script src="<?php echo base_url("assets/js/metro.js");?>"></script>
+    <script src="<?php echo base_url("assets/js/metro.min.js");?>"></script>
+    <!-- End Javascript-->
+
+    <meta charset="utf-8">
+    <title>Untitled Document</title>
+</head>
+
+<body class="container">
+   <div class="grid">
+    <div class="row">
+        <div class="cell">
+            <form action="upload/upload_file" class="dropzone" id="my-dropzone">
+
+            </form>
+            <!-- <div class="dropzone" id="my-dropzone"></div> -->
+
+            <button id="submit-all">Submit all files</button>
         </div>
     </div>
-    <!-- The table listing the files available for upload/download -->
-    <table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
-</form>
-<!-- The blueimp Gallery widget -->
-<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-filter=":even">
-    <div class="slides"></div>
-    <h3 class="title"></h3>
-    <a class="prev">‹</a>
-    <a class="next">›</a>
-    <a class="close">×</a>
-    <a class="play-pause"></a>
-    <ol class="indicator"></ol>
+    <div class="row">
+        <div class="cell">
+            <div class="set-border padding10 no-padding-top no-padding-bottom">
+              <label class="input-control radio small-check">
+                  <input type="radio" name="r1" value="default" checked="">
+                  <span class="check"></span>
+                  <span class="caption">default</span>
+              </label>
+              <label class="input-control radio small-check">
+                  <input type="radio" name="r1" value="list-type-icons">
+                  <span class="check"></span>
+                  <span class="caption">Icons</span>
+              </label>
+              <label class="input-control radio small-check">
+                  <input type="radio" name="r1" value="list-type-tiles">
+                  <span class="check"></span>
+                  <span class="caption">Tiles</span>
+              </label>
+              <label class="input-control radio small-check">
+                  <input type="radio" name="r1" value="list-type-listing">
+                  <span class="check"></span>
+                  <span class="caption">Listing</span>
+              </label>
+          </div>
+          <div class="listview set-border padding10 default" data-role="listview" id="lv1" >
+
+          </div>
+      </div>
+  </div>
 </div>
-<!-- The template to display files available for upload -->
-<script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-upload fade">
-        <td>
-            <span class="preview"></span>
-        </td>
-        <td>
-            <p class="name">{%=file.name%}</p>
-            <strong class="error text-danger"></strong>
-        </td>
-        <td>
-            <p class="size">Processing...</p>
-            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-        </td>
-        <td>
-            {% if (!i && !o.options.autoUpload) { %}
-                <button class="btn btn-primary start" disabled>
-                    <i class="glyphicon glyphicon-upload"></i>
-                    <span>Start</span>
-                </button>
-            {% } %}
-            {% if (!i) { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
-{% } %}
+</body>
+<script type="text/javascript">
+    $(document).ready(function(e) {
+
+
+
+
+     Dropzone.options.myDropzone = {
+
+  // Prevents Dropzone from uploading dropped files immediately
+  autoProcessQueue: false,
+
+  init: function() {
+    var submitButton = document.querySelector("#submit-all")
+        myDropzone = this; // closure
+
+        submitButton.addEventListener("click", function() {
+      myDropzone.processQueue(); // Tell Dropzone to process all queued files.
+  });
+
+    // You might want to show the submit button only when 
+    // files are dropped here:
+    this.on("addedfile", function() {
+      // Show submit button here and/or inform user to click it.
+  });
+
+}
+};
+
+
+
+     var dropzone = new Dropzone('.dropzone', {
+        parallelUploads: 2,
+        acceptedFiles: 'image/*,.pdf,.avi,.mp4,.flv',
+        filesizeBase: 1024,
+        maxFilesize:25000,
+        createImageThumbnails: true,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Drop files here to upload",
+        dictFallbackMessage: "Browser anda tidak suport drag'n'drop file upload.",
+        dictFileTooBig: "Ukuran file terlalu besar Max ukuran file: {{maxFilesize}}Mb.",
+        dictInvalidFileType: "You can't upload files of this type.",
+        dictResponseError: "Server responded with {{statusCode}} code.",
+        dictCancelUpload: "Cancel upload",
+        dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+        dictRemoveFile: "Remove file",
+        dictRemoveFileConfirmation: null
+    });
+
+
+
+
+
+        // dropzone.on("complete", function(file) {
+        //     //get_file();
+        //     var int = setTimeout(function(){
+        //        dropzone.removeFile(file);},
+        //        5000);
+        // });
+        // get_file();
+    });
+
+
+
+
+    // function get_file()
+    // {
+    //     $.ajax({
+    //         url:"<?php echo base_url()?>upload/get_file",
+    //         success: function(data)
+    //         {
+    //             var dt = JSON.parse(data)
+    //             $("#lv1").html(dt.content);
+    //         }
+    //     });
+    // }
 </script>
-<!-- The template to display files available for download -->
-<script id="template-download" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-download fade">
-        <td>
-            <span class="preview">
-                {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-                {% } %}
-            </span>
-        </td>
-        <td>
-            <p class="name">
-                {% if (file.url) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-                {% } else { %}
-                    <span>{%=file.name%}</span>
-                {% } %}
-            </p>
-            {% if (file.error) { %}
-                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-        <td>
-            <span class="size">{%=o.formatFileSize(file.size)%}</span>
-        </td>
-        <td>
-            {% if (file.deleteUrl) { %}
-                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                    <i class="glyphicon glyphicon-trash"></i>
-                    <span>Delete</span>
-                </button>
-                <input type="checkbox" name="delete" value="1" class="toggle">
-            {% } else { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
-{% } %}
-</script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="assets/js/fileupload/vendor/jquery.ui.widget.js"></script>
-<!-- The Templates plugin is included to render the upload/download listings -->
-<script src="https://blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
-<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="https://blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
-<!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="https://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<!-- blueimp Gallery script -->
-<script src="https://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
-<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="assets/js/fileupload/jquery.iframe-transport.js"></script>
-<!-- The basic File Upload plugin -->
-<script src="assets/js/fileupload/jquery.fileupload.js"></script>
-<!-- The File Upload processing plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-process.js"></script>
-<!-- The File Upload image preview & resize plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-image.js"></script>
-<!-- The File Upload audio preview plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-audio.js"></script>
-<!-- The File Upload video preview plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-video.js"></script>
-<!-- The File Upload validation plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-validate.js"></script>
-<!-- The File Upload user interface plugin -->
-<script src="assets/js/fileupload/jquery.fileupload-ui.js"></script>
-<!-- The main application script -->
-<script src="assets/js/fileupload/main.js"></script>
-<!-- The XDomainRequest Transport is included for cross-domain file deletion for IE 8 and IE 9 -->
-<!--[if (gte IE 8)&(lt IE 10)]>
-<script src="assets/js/fileupload/cors/jquery.xdr-transport.js"></script>
-<![endif]-->
+</html>
